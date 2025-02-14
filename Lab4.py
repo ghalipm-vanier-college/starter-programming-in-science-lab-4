cacd6e70d411baa35f07eca5930c7be57e63d036
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-02-13 20:20:03 -0500

    Create Lab4.py
    method frames provided

---
+++ Lab4.py
@@ -0,0 +1,10 @@
+def check_weekday_or_weekend(day_number):
+    """Check if the given number is a proper day number and determine if it's a Weekday or Weekend."""
+    # TODO: Implement this function
+    pass  # Placeholder statement
+
+
+def get_day_name(day_number):
+    """Check if the given number is a proper day number and return the corresponding day name."""
+    # TODO: Implement this function
+    pass  # Placeholder statement
