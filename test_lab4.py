859ae07d884cb2e45c40a53e62c6780992cd463f
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-09-18 00:38:20 -0400

    Update test_lab4.py
    test file update commit

--- test_lab4.py
+++ test_lab4.py
@@ -1,27 +1,30 @@
 # DO NOT CHANGE THIS FILE. IF YOU DO, YOU GET ZERO FOR THE ASSIGNMENT
 import unittest
-from Lab4 import check_weekday_or_weekend, get_day_name
+from Lab4 import check_weekday_or_weekend, get_day_name, get_letter_grade
+
 
 # Test check_weekday_or_weekend function
 def test_check_weekday_or_weekend():
     assert check_weekday_or_weekend(0) == "Not a proper day number!"
-    assert check_weekday_or_weekend(2) == "It is a Weekday!"
-    assert check_weekday_or_weekend(5) == "It is a Weekday!"
-    assert check_weekday_or_weekend(6) == "It is a Weekend!"
-    assert check_weekday_or_weekend(7) == "It is a Weekend!"
+    assert check_weekday_or_weekend(2) == "Weekday"
+    assert check_weekday_or_weekend(5) == "Weekday"
+    assert check_weekday_or_weekend(6) == "Weekday"
+    assert check_weekday_or_weekend(7) == "Weekday"
     assert check_weekday_or_weekend(8) == "Not a proper day number!"
+
 
 # Test get_day_name function
 def test_get_day_name():
     assert get_day_name(0) == "Not a proper day number!"
-    assert get_day_name(1) == "It is a Monday"
-    assert get_day_name(2) == "It is a Tuesday"
-    assert get_day_name(3) == "It is a Wednesday"
-    assert get_day_name(4) == "It is a Thursday"
-    assert get_day_name(5) == "It is a Friday"
-    assert get_day_name(6) == "It is a Saturday"
-    assert get_day_name(7) == "It is a Sunday"
+    assert get_day_name(1) == "Monday"
+    assert get_day_name(2) == "Tuesday"
+    assert get_day_name(3) == "Wednesday"
+    assert get_day_name(4) == "Thursday"
+    assert get_day_name(5) == "Friday"
+    assert get_day_name(6) == "Saturday"
+    assert get_day_name(7) == "Sunday"
     assert get_day_name(8) == "Not a proper day number!"
+
 
 # Test get_letter_grade function
 def test_get_letter_grade():
