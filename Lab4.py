c5c5adaefbd6a2fae6f1f415afc4c918afa66b72
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-09-17 23:23:51 -0400

    Update Lab4.py
    question added commit

--- Lab4.py
+++ Lab4.py
@@ -8,3 +8,9 @@ def get_day_name(day_number):
     """Check if the given number is a proper day number and return the corresponding day name."""
     # TODO: Implement this function
     pass  # Placeholder statement
+
+
+def get_letter_grade(mark):
+    """Check if the given mark is valid and return the corresponding letter grade."""
+    # TODO: Implement this function
+    pass  # Placeholder statement
